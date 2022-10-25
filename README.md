f1b8ccd63d2f90b8734ff7108f1d376b44a4ad73
Quan Tran <quantran@lecle.co.kr>
2022-10-25 13:53:00 +0700

    feat: init commit

---
+++ README.md
@@ -0,0 +1,40 @@
+# google-image-cli
+
+google-image-cli is a cli toolkit to search using google and show 20 images to cli.
+
+## Installation
+
+Run with npx
+
+```bash
+  npx google-image-cli -n number text-to-search
+```
+
+`-n` : number of images (max 20 images)
+
+## Example
+Search 2 pics of dog.
+```bash
+  npx google-image-cli -n 2 dog
+```
+
+![dog](wiki/dog.png)
+
+Search cat image.
+```bash
+  npx google-image-cli cat
+```
+
+![cat](wiki/cat.png)
+
+## Contributing
+
+All contributions are accepted as a PR.
+
+* You can file issues by submitting a PR.
+* Implement new feature by submitting a PR.
+* Improve documentation by submitting PR.
+You are welcome to improve this project! It would help me so much!
+## Support
+
+For support, email downy1229@gmail.com!
---
+++ src/cli.js
@@ -0,0 +1,33 @@
+import got from 'got';
+import terminalImage from 'terminal-image';
+import arg from "arg";
+export async function cli(rawArgs) {
+    const args = arg({
+        "-n": Number,
+    }, {
+        argv: rawArgs.slice(2),
+    });
+    const n = 0;
+    if (args[-n]) {
+        if (args[-n] < 1) {
+            n = 0;
+        } else if (args[-n] > 20) {
+            n = 20;
+        }
+    }
+    const searchTerm = args['_'].join(' ');
+    if (searchTerm) {
+        console.log(`\x1b[94;1mSearching images for ${searchTerm}....\x1b[0m`);
+        const data = await got.get(`https://www.google.com/search?tbm=isch&sclient=img&q=${searchTerm}`);
+        const html = data.rawBody.toString('utf-8');
+        const listBuffer = await Promise.all(html.match(/"https:\/\/encrypted-tbn0\.gstatic\.com\/images\?\S+"/g).slice(0,n + 1).map(e => e.substring(1, e.length - 1)).map(e => new Promise(async (res, rej) => {
+            const body = await got(e).buffer();
+            res(body)
+        })));
+        for (let img of listBuffer) {
+            console.log(await terminalImage.buffer(img))
+        }
+    } else {
+        console.log("Please input search text")
+    }
+}
